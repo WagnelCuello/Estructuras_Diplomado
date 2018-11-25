f4bb420e33f54975e82f2fb5f9278ceb66fde5e6
wagnel <wagnelcuello@gmail.com>
2018-11-25 16:49:13 -0400

    Inicial de la clase

--- Estructuras_Diplomado/Program.cs
+++ Estructuras_Diplomado/Program.cs
@@ -10,6 +10,37 @@ namespace Estructuras_Diplomado
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.GetProblema1();
+        }
+
+        /// <summary>
+        /// este metodo se encarga de mostrar numeros multiplos del 1 al 200
+        /// </summary>
+        private void GetProblema1()
+        {
+            Console.WriteLine("===> NUMEROS MULTIPLOS DE 3 DEL 1 AL 200 <===");
+
+            int numero = 1;
+
+            while (numero <= 200)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+                numero++;
+            }
+            Console.WriteLine("-----------------------------");
+            Console.Write("Presione una tecla...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// este metodo se encarga de 
+        /// </summary>
+        private void GetProblema2()
+        {
 
         }
     }
